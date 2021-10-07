4efcc2044730274f76f241271d6b6e119fa9ef68
LT-6FGD373\Hannes <h.roever@zertificon.com>
2021-10-07 21:44:06 +0200

    Initial commit

---
+++ FailDaysClientApp/Models/Student.cs
@@ -0,0 +1,10 @@
+﻿namespace FailDaysClientApp.Models
+{
+    public class Student
+    {
+        public int MatNr { get; set; }
+        public int CourseId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
